491e9ecd2d46c7c8aa0c8824994765e39f276db3
byukim <kby9906@naver.com>
2023-08-22 11:33:19 +0900

    [ADD] openGraph title과 description 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,9 @@ const darkTheme = createTheme({
 });
 
 const inter = Inter({ subsets: ['latin'] });
+const title = '스파이폴 온라인: 가입 없이 무료로 스파이폴 즐기기';
+const descriptionContent =
+  '보드게임 스파이폴을 가입 없이 온라인으로 즐길 수 있는 사이트입니다. PC든 모바일이든 어디서나 간편하게 스파이폴을 즐겨보세요!';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -31,11 +34,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <CssBaseline />
         <html lang="ko">
           <head>
-            <title>스파이폴 온라인: 가입 없이 무료로 스파이폴 즐기기</title>
+            <title>{title}</title>
             <script
               async
               src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1473493437844810"
               crossOrigin="anonymous"></script>
+            <meta name="description" content={descriptionContent} />
+            <meta property="og:title" content={title} />
+            <meta property="og:description" content={descriptionContent} />
             <meta name="naver-site-verification" content="6ee3bb7b2745a9d34ef088afda8f091c6d6f14b2" />
           </head>
           <body className={inter.className}>
