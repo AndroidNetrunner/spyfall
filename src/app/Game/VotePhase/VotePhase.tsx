5e84343161ea013c69cb6af450525daa1578adb1
byukim <kby9906@naver.com>
2023-12-10 00:28:30 +0900

    [FIX] 제한시간 종료 후에도 새로운 고발이 들어오면 투표 가능

--- src/app/Game/VotePhase/VotePhase.tsx
+++ src/app/Game/VotePhase/VotePhase.tsx
@@ -3,12 +3,17 @@ import VoteForm from './VoteForm';
 import PlayersWhoDidNotVote from '../../components/PlayersWhoDidNotVote';
 import { useVotePhase } from '@/app/hooks/useVoteState';
 import { useState } from 'react';
+import { selectNominator } from '@/redux/slices/questionPhaseSlice';
+import { useSelector } from 'react-redux';
+import VoteForAccuse from '../QuestionPhase/VoteForAccuse';
 
 export default function VotePhase() {
   const { votedTo, setVotedTo, unvotedOpponents } = useVotePhase();
   const [hasVote, setHasVote] = useState(true);
+  const nominator = useSelector(selectNominator);
   return (
     <>
+      {nominator && <VoteForAccuse />}
       {hasVote ? (
         <VoteForm votedTo={votedTo} setVotedTo={setVotedTo} setHasVote={setHasVote} />
       ) : unvotedOpponents.length > 0 ? (
